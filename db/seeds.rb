3694a5b341de31411d52e5059810bd876f1e37d8
Marcus Vinicius da Silva Borges <marcus.f22@gmail.com>
2020-03-24 22:40:32 -0300

    chore: creates 3 basic tickets in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Ticket.create([
+  {
+    title: 'Deal 1572 não visualiza tela de acompanhamento',
+    description: 'Entrou pela ESI Z. Após enviar os docs leva tomadinha na tela de acompanhamento'
+  },
+  {
+    title: 'Não consigo gerar CCB pro deal 1602',
+    description: 'Link do deal: www.pudim.com.br. Número da proposta: 123456789'
+  },
+  {
+    title: 'Lentidão no Pipedrive :(',
+    description: ''
+  }
+])
